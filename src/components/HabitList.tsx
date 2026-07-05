7131e6b8b79bfb82a89f702c1710e44dbf9c391c
dknight <gnub33@gmail.com>
2026-07-05 15:23:42 -0400

    visible habit items

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,12 +1,47 @@
+import Button from "./Button"
+
 export function HabitList() {
-    const habits = []
+    const habits = [
+        { id: "1", name: "Hi"}, 
+        { id: "2", name: "Bye"}
+    ]
 
     if (habits.length === 0 ) {
         return (<p className="text-center text-zinc-500 py-12">No habits yet. Add one above to get started!</p>)
     }
 
-    return <h1>Full</h1>
-
-
+    return (
+        <div className="flex flex-col gap-3">
+        {habits.map(habit => (
+            //when looping through array to render elements, always add
+            // UNIQUE key property, usually an id
+           <HabitItem key={habit.id} habit={habit} />
+        ))}
+    </div>
+    )
 }
 
+type HabitItemProps = {
+    habit: { id: string, name: string }
+}
+
+function HabitItem( {habit}: HabitItemProps) {
+    const visibleDates = [new Date()]
+    return <div className="rounded-xl bg-zinc-800 p-4 flex flex-col gap-3">
+        <div className="flex items-center justify-between">
+            <div className="flex gap-3 items-center">
+                <span className="font-medium">{habit.name}</span>
+                <span className="text-sm text-amber-400">🔥 3</span>
+            </div>
+            <Button>Delete</Button>
+        </div>
+        <div className="flex gap-1.5">
+            {visibleDates.map(date => (
+                <Button key={date.toISOString()}>
+                    <span className="font-medium"></span>
+                </Button>
+            ))}
+        </div>
+    </div>
+}
+ 
