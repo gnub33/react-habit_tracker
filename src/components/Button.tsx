95eea85c1b602667e008baf11bdf93ace9e476d4
dknight <gnub33@gmail.com>
2026-07-05 01:50:31 -0400

    form placeholder

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 
 export default function Button({ children }: ButtonProps) {
     return (
-        <button className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity=-30 disabled:cursor-not-allowed">{children}</button>
+        <button className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed">{children}</button>
     )
 }
 
---
+++ src/components/HabitList.tsx
@@ -0,0 +1,12 @@
+export function HabitList() {
+    const habits = []
+
+    if (habits.length === 0 ) {
+        return (<p className="text-center text-zinc-500 py-12">No habits yet. Add one above to get started!</p>)
+    }
+
+    return <h1>Full</h1>
+
+
+}
+
